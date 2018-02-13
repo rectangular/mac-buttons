be378f8c094d164eaea02ab7e3847e14adb5895d
Geof Crowl <rectangular@gmail.com>
2018-02-12 20:40:50 -0700

    Initial Commit

---
+++ Mac Button Test/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Mac Button Test
+//
+//  Created by Geof Crowl on 2/12/18.
+//  Copyright © 2018 Geof Crowl. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
